00a0cb344f79255d68ca2268fe9ed0c1b430510b
ics-kitagawa <kitagawa@ics-web.jp>
2025-11-13 13:53:43 +0900

    build設定の修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: "/251113_three-postprocessing/",
+  base: "./",
   build: {
     outDir: "docs",
     rollupOptions: {
